ceefc884cedb5045556df8fd44815c416b7cf0f1
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2018-09-13 23:45:47 +0200

    Update SQLite_introduction.py

--- SQLite_introduction.py
+++ SQLite_introduction.py
@@ -1,14 +1,75 @@
 import sqlite3
 
+conn = sqlite3.connect('music.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Tracks')
+cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
+
+conn.close()
+
+
+
+
+
+import sqlite3
+
+conn = sqlite3.connect('music.sqlite')
+cur = conn.cursor()
+
+cur.execute('INSERT INTO Tracks (title, plays) VALUES (?, ?)', 
+    ('Thunderstruck', 20))
+cur.execute('INSERT INTO Tracks (title, plays) VALUES (?, ?)', 
+    ('My Way', 15))
+conn.commit()
+
+print('Tracks:')
+cur.execute('SELECT title, plays FROM Tracks')
+for row in cur:
+     print(row)
+
+cur.execute('DELETE FROM Tracks WHERE plays < 100')
+
+cur.close()
+
+
+
+
+
+
+
+cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ? )''', 
+        ( name, album_id, length, rating, count ) )
+
+    conn.commit()
+
+
+import sqlite3
+
 conn = sqlite3.connect('my_db.sqlite')
 cur = conn.cursor()
 
-# Do some setup
 cur.executescript('''
 DROP TABLE IF EXISTS User;
-
 
 CREATE TABLE User (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name   TEXT UNIQUE);
 ''')
+
+
+
+
+
