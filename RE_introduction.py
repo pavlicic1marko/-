75f3e093d4432747fbb0a698c28c299efdbee04e
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2018-09-13 23:22:27 +0200

    Update RE_introduction.py

--- RE_introduction.py
+++ RE_introduction.py
@@ -4,3 +4,8 @@ for line in text:
     line = line.rstrip()
     if re.search('ovo je bitno', line): #ako postoji ovaj string ,vraca True
         print(line)
+    if re.search('^ovo je bitno', line): #ako pocinje
+        print(line)
+    print( re.findall('[gG]', line))#vraca listu,praznu ili sa nekoliko elemenata
+
+
