f72bd27878e2470ab378def54ecb76671d30f513
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2018-09-13 23:34:26 +0200

    Update Download_introduction.py

--- Download_introduction.py
+++ Download_introduction.py
@@ -1,2 +1,8 @@
 import urllib.request
 urllib.request.urlretrieve("https://grad.ucla.edu/asis/agep/advcv.pdf","new.pdf")
+
+import urllib.request, urllib.parse, urllib.error
+img = urllib.request.urlopen('https://viphouse.rs/products/27031/6360.jpg').read()
+fhand = open('6360.jpg', 'wb')
+fhand.write(img)
+fhand.close()
