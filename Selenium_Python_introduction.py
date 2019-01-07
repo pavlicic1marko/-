b1a67d526fde72a1e77915b89b1e538382f7e281
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2019-01-07 17:30:30 +0100

    Update Selenium_Python_introduction.py

--- Selenium_Python_introduction.py
+++ Selenium_Python_introduction.py
@@ -9,3 +9,29 @@ time.sleep(3)
 driver.get_screenshot_as_file("youtestme.png")
 time.sleep(3)
 driver.quit()
+
+
+__author__="ME"
+import time
+from selenium import webdriver
+driver=webdriver.Chrome("D:\\chromedriver.exe") #location of the fail
+driver.set_page_load_timeout(30)
+driver.get("https://google.com")
+driver.find_element_by_name("q").send_keys("Automation")  #q is the valu for the name atribute of the search bar
+time.sleep(4)
+driver.find_element_by_name("btnK").click()
+time.sleep(4)
+driver.quit()
+
+
+
+__author__="ME"
+import time
+import re
+from selenium import webdriver
+driver=webdriver.Chrome("D:\\chromedriver.exe") #location of the fail
+driver.set_page_load_timeout(30)
+driver.get("https://google.com")
+doc=driver.page_source
+print(doc)
+
