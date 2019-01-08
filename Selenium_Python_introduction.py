e5a4f08de456ddf14803c92adde671bce81870e9
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2019-01-08 22:02:09 +0100

    Update Selenium_Python_introduction.py

--- Selenium_Python_introduction.py
+++ Selenium_Python_introduction.py
@@ -35,3 +35,21 @@ driver.get("https://google.com")
 doc=driver.page_source
 print(doc)
 
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver=webdriver.Chrome("G:\\chromedriver_win32\\chromedriver.exe") #location of the fail
+driver.set_page_load_timeout(30)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+print(elem)
+elem.clear()
+print(elem)
+elem.send_keys("pycon")
+print(elem)
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+print(elem[1])
+driver.close()
